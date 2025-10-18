66307fb53f2d6cd7a65b7d7c1c7d538a76de2906
Jakub-Ignatowicz <kuba_ign@proton.me>
2025-10-18 19:54:24 +0200

    feat(nvim): treesitter colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,38 @@
 local M = {
-  "LunarVim/darkplus.nvim",
-  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+	"LunarVim/darkplus.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd([[
+	vim.cmd([[
   hi Normal guibg=NONE ctermbg=NONE
   hi NormalNC guibg=NONE ctermbg=NONE
   hi VertSplit guibg=NONE ctermbg=NONE
   hi StatusLine guibg=NONE ctermbg=NONE
   hi LineNr guibg=NONE ctermbg=NONE
-]])
+  ]])
+
+	vim.api.nvim_set_hl(0, "@function", { fg = "#E0AF68", bold = true })
+	vim.api.nvim_set_hl(0, "@type", { fg = "#A3BE8C", italic = true })
+	vim.api.nvim_set_hl(0, "@keyword", { fg = "#D08770", bold = true })
+	vim.api.nvim_set_hl(0, "@variable", { fg = "#E6B980" })
+	vim.api.nvim_set_hl(0, "@variable.builtin", { fg = "#B48EAD", italic = true })
+	vim.api.nvim_set_hl(0, "@constant", { fg = "#EBCB8B" })
+	vim.api.nvim_set_hl(0, "@number", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "@boolean", { fg = "#D19A66", bold = true })
+	vim.api.nvim_set_hl(0, "@string", { fg = "#A7C080" })
+	vim.api.nvim_set_hl(0, "@comment", { fg = "#928374", italic = true })
+	vim.api.nvim_set_hl(0, "@operator", { fg = "#EBCB8B" })
+	vim.api.nvim_set_hl(0, "@punctuation", { fg = "#D8C3A5" })
+
+	vim.api.nvim_set_hl(0, "@field", { fg = "#E6B980" })
+	vim.api.nvim_set_hl(0, "@property", { fg = "#E6B980" })
+	vim.api.nvim_set_hl(0, "@variable.parameter", { fg = "#C0A888" })
+	vim.api.nvim_set_hl(0, "@namespace", { fg = "#A3BE8C" })
+	vim.api.nvim_set_hl(0, "@module", { fg = "#A3BE8C" })
+	vim.api.nvim_set_hl(0, "@function.builtin", { fg = "#E0AF68", bold = true })
+	vim.api.nvim_set_hl(0, "@keyword.function", { fg = "#D08770", bold = true })
 end
 
 return M
